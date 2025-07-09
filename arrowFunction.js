6e15bb351bad6fb766f3cb2740a64ac88c50ada3
Gabriel Barbosa Santos <gabrielsantos.gbs@outlook.com>
2025-07-09 20:44:33 -0300

    arrow functions

---
+++ arrowFunction.js
@@ -0,0 +1,20 @@
+//Declaração de uma função usando arrow function
+// A sintaxe é mais curta e não usa a palavra-chave "function".
+
+let funcao = (x) => {
+  return x * 2;
+};
+
+//Os parenteses são opcionais quando há apenas um parâmetro.
+// Se houver mais de um parâmetro, os parênteses são obrigatórios.
+// Se não houver parâmetros, deve-se usar parênteses vazios.
+// A palavra-chave "return" é opcional se a função for de uma única linha.
+let funcao1 = (x) => x * 2;
+// Se houver mais de uma linha, deve-se usar chaves e a palavra-chave "return".
+let funcao2 = (x) => {
+  let y = x * 2;
+  return y;
+};
+
+//mais sobre funções anônimas:
+//https://chatgpt.com/share/686efdb2-febc-8004-92c9-1578926061ce
